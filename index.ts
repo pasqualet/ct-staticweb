0290c5838bfa7e557ccadea9f7a35bbe4acc7cba
Pasquale Toscano <pasquale.toscano@astrokube.com>
2022-11-26 18:16:54 +0100

    Initial commit

---
+++ index.ts
@@ -0,0 +1,9 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as ct from "@cloudtoolkit/aws";
+
+const config = new pulumi.Config();
+const domain = config.require("domain");
+const staticweb = new ct.serverless.StaticWeb("name", {
+  domain: domain,
+  configureDNS: true,
+});
